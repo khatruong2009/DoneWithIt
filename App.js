11aa51cd76f46bdf638aa5cfb09b8ee1209034c2
Kha Truong <64438356+khatruong2009@users.noreply.github.com>
2021-02-23 16:59:00 -0500

    added all inputs and tested first piece of functionality

--- App.js
+++ App.js
@@ -1,33 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button, SafeAreaView, Image, TextInput, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView, Image, TextInput, TouchableWithoutFeedback, Keyboard, Picker } from 'react-native';
 
 export default function App() {
 
   const [bill, setBill] = useState(0);
+  const [tip, setTip] = useState(0);
+  const [people, setPeople] = useState(0);
 
-  // const DismissKeyboard = ({children}) => (
-  //   <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-  //     {children}
-  //   </TouchableWithoutFeedback>
-  // )
+  const [resultTip, setResultTip] = useState();
+
+  let resultTotal = 0;
+  let resultPerson = 0;
+
+  const handlePress = () => {
+    setResultTip(bill * (tip/100));
+    console.log(resultTip);
+  }
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+
+      {/* main container */}
       <SafeAreaView style={styles.container}>
+
         <Image source={{width: 200, height: 200, uri: "https://picsum.photos/id/1025/200/300"}}></Image>
-        <Text style={{fontWeight: "bold"}}>Tip Calculator</Text>
-        <Text>Full Bill Amount: </Text>
+
+        <Text style={{fontWeight: "bold", fontSize: 30}} maxFontSizeMultiplier={2}>Tip Calculator</Text>
+
+        {/* Input Bill Amount */}
+        <Text style={styles.inputHeader}>Full Bill Amount: </Text>
+
         <TextInput 
           type={"number"} 
           style={styles.input} 
           keyboardType={'decimal-pad'} 
           onChangeText={(number) => {setBill(number)}}>  
         </TextInput>
-        <Text>{bill}</Text>
-        <Button onPress={() => setCounter(counter + 1)} title="Tap Me"></Button>
+
+        {/* Input Tip Percentage */}
+        <Text style={styles.inputHeader}>Tip Percentage: </Text>
+
+        <TextInput 
+          type={"number"} 
+          style={styles.input} 
+          keyboardType={'decimal-pad'} 
+          onChangeText={(percent) => {setTip(percent)}}>  
+        </TextInput>
+
+        {/* Input Number of People */}
+        <Text style={styles.inputHeader}>Number of People: </Text>
+
+        <TextInput 
+          type={"number"} 
+          style={styles.input} 
+          keyboardType={'decimal-pad'} 
+          onChangeText={(number) => {setPeople(number)}}>  
+        </TextInput>
+
+        <Text>{resultTip}</Text>
+
+        <Button onPress={handlePress} title="Calculate"></Button>
+
         <StatusBar style="auto" />
+
       </SafeAreaView>
+
     </TouchableWithoutFeedback>
     
   );
@@ -39,6 +77,10 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  inputHeader: {
+    paddingTop: 15,
+    fontSize: 20,
   },
   input: {
     borderWidth: 1,
